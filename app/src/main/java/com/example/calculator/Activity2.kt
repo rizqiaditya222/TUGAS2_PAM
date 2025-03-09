9c170b586d16a20162835abe4195ae8a64d30aeb
Rizqi <rizqiaditya222@gmail.com>
2025-03-09 18:44:26 +0700

    fix bug

--- app/src/main/java/com/example/calculator/Activity2.kt
+++ app/src/main/java/com/example/calculator/Activity2.kt
@@ -14,7 +14,7 @@ class Activity2 : AppCompatActivity() {
         val tvNIM = findViewById<TextView>(R.id.tvNIM)
         val tvNama = findViewById<TextView>(R.id.tvNama)
 
-        val result = intent.getDoubleExtra("RESULT", 0.0)
+        val result = intent.getDoubleExtra("HASIL", 0.0)
         val nim = intent.getStringExtra("NIM") ?: "NIM tidak diketahui"
         val nama = intent.getStringExtra("NAMA") ?: "Nama tidak diketahui"
 
